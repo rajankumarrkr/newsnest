02ea7998e50c77b6619ba73968ffcfd69d097f98
rajankumarrkr <rajankumarrkr4576@gmail.com>
2026-05-07 10:39:15 +0530

    update cors

--- server/server.js
+++ server/server.js
@@ -15,15 +15,7 @@ scraperNews();
 
 const app = express();
 
-app.use(
-    cors({
-        origin: [
-            'http://localhost:5173',
-            'https://newsnest-rouge.vercel.app/',
-        ],
-        credentials: true,
-    })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
